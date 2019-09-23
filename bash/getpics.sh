aa61485afbdf50dd5bdc566d71b2c3da7f92c58d
jordankanowitz <jordan.kanowitz@gmail.com>
2019-09-22 22:03:49 -0400

    for lab 2

--- bash/getpics.sh
+++ bash/getpics.sh
@@ -17,6 +17,16 @@ test -f ~/Pictures/pics.zip || wget -q -O ~/Pictures/pics.zip http://zonzorp.net
 # unpack the downloaded zipfile if it is there, then delete the local copy of the zipfile
 test -f ~/Pictures/pics.zip && unzip -d ~/Pictures -o -q ~/Pictures/pics.zip && rm ~/Pictures/pics.zip
 
+
+
+#modify the script to also get the .tgz file
+#download the .tgz file to the pictures directory if it isn't already there
+test -f ~/Pictures/pics.tgz || wget -q -O ~/Pictures/pics.tgz http://zonzorp.net/pics.tgz
+
+#unpack the downloaded tgz file to Pictures directory and if it is there delete a local copy of zip file
+test -f ~/Pictures/pics.tgz && tar -xzf ~/Pictures/pics.tgz -C ~/Pictures && rm ~/Pictures/pics.tgz
+
+#cut this part and moved it to end of script
 # Make a report on what we have in the Pictures directory
 test -d ~/Pictures && cat <<EOF
 Found $(find ~/Pictures -type f|wc -l) files in the Pictures directory.
